6458877e4bc28a42628a5174e6624b7f740507ea
Craig Condit <ccondit@apache.org>
2024-01-05 17:51:15 -0600

    [WIP] Start of history server tsdb

---
+++ pkg/cmd/historyserver/main.go
@@ -0,0 +1,133 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/apache/yunikorn-core/pkg/log"
+	"github.com/apache/yunikorn-k8shim/pkg/historyserver"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+const Metric = "m"
+const ApplicationID = "a"
+const TaskID = "t"
+const NodeID = "n"
+const InstanceTypeID = "i"
+const DurationSec = "d"
+const ResourceType = "r"
+
+func main() {
+	log.Log(log.Test).Info("START")
+	test()
+	log.Log(log.Test).Info("END")
+}
+
+func test() {
+	// create some in-memory dictionaries
+	appIDs := historyserver.NewDictionary()
+	taskIDs := historyserver.NewDictionary()
+	taskNames := historyserver.NewDictionary()
+	userNames := historyserver.NewDictionary()
+	nodeNames := historyserver.NewDictionary()
+	instanceTypes := historyserver.NewDictionary()
+	resourceIDs := historyserver.NewDictionary()
+
+	// create some dummy data
+	users := []string{"tom", "dick", "harry", "alice", "bob"}
+	nodes := []string{"node0001.example.com", "node0002.example.com", "node0003.example.com", "node0004.example.com"}
+	instances := []string{"normal", "normal", "normal", "gpu"}
+	resourceIDs.AddSymbol("vcore")
+	resourceIDs.AddSymbol("memory")
+	resourceIDs.AddSymbol("pods")
+	resourceIDs.AddSymbol("nvidia.com/gpu")
+	resourceIDs.AddSymbol("ephemeral-storage")
+	resourceIDs.AddSymbol("hugepages-1Gi")
+	resourceIDs.AddSymbol("hugepages-2Mi")
+
+	for i := 0; i < 1000; i++ {
+		log.Log(log.Test).Info("Batch", zap.Int("i", i))
+		for j := 0; j < 100; j++ {
+			applicationID := fmt.Sprintf("application_%04d", j)
+			taskID := uuid.New().String()
+			taskName := fmt.Sprintf("task_%04d_%06d", j, i)
+			userName := users[j%5]
+			nodeName := nodes[i%4]
+			instanceType := instances[i%4]
+
+			// register the symbols as we come across them
+			appIDs.AddSymbol(applicationID)
+			taskIDs.AddSymbol(taskID)
+			taskNames.AddSymbol(taskName)
+			userNames.AddSymbol(userName)
+			nodeNames.AddSymbol(nodeName)
+			instanceTypes.AddSymbol(instanceType)
+		}
+	}
+
+	sortedAppIDs := appIDs.Sort()
+	if err := sortedAppIDs.Save("/tmp/historyserver/dict-appid.dat"); err != nil {
+		panic(err)
+	}
+	sortedTaskIDs := taskIDs.Sort()
+	if err := sortedTaskIDs.Save("/tmp/historyserver/dict-taskid.dat"); err != nil {
+		panic(err)
+	}
+	sortedTaskNames := taskNames.Sort()
+	if err := sortedTaskNames.Save("/tmp/historyserver/dict-taskname.dat"); err != nil {
+		panic(err)
+	}
+	sortedUserNames := userNames.Sort()
+	if err := sortedUserNames.Save("/tmp/historyserver/dict-username.dat"); err != nil {
+		panic(err)
+	}
+	sortedNodeNames := nodeNames.Sort()
+	if err := sortedNodeNames.Save("/tmp/historyserver/dict-nodename.dat"); err != nil {
+		panic(err)
+	}
+	sortedInstanceTypes := instanceTypes.Sort()
+	if err := sortedInstanceTypes.Save("/tmp/historyserver/dict-instancetype.dat"); err != nil {
+		panic(err)
+	}
+	sortedResourceIDs := resourceIDs.Sort()
+	if err := sortedResourceIDs.Save("/tmp/historyserver/dict-resourceid.dat"); err != nil {
+		panic(err)
+	}
+
+	// once the sorted dictionaries are created, we will need to update the data structures we have created with
+	// the new IDs; this should just involve iterating each entry and looking up the existing key in the old dictionary
+	// and then mapping it to the new ID in the sorted dictionary.
+
+	// creating an in-memory DB that is both appendable and queryable efficiently may be a bit challenging. The indexes
+	// will not be sorted until we finalize what we have to disk. While reconciling, the entire DB needs to be
+	// frozen, as it will not be consistent until the process completes.
+
+	// we can create multiple shards per partition, and write a new shard once our data size has grown too large.
+	// additionally if we have no open shards in a partition, we can create a new one and keep it open for a period of
+	// time (say 5 minutes or so).
+
+	// if the number of shards grows too large, we can merge them with a background process and then atomically replace
+	// the shards with the merged data. this should help ensure that we don't consume too much RAM.
+
+	// once a shard is "finalized", it becomes read-only and we can use mmap'd i/o to read from it. we need store only
+	// the dictionaries in memory (which will be very small).
+}
---
+++ pkg/historyserver/dictionary.go
@@ -0,0 +1,191 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package historyserver
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+	"sort"
+	"sync"
+)
+
+const DictionaryV1 = "YK_DICT_V1"
+
+var DictionaryV1Len = []byte{byte(len(DictionaryV1))}
+
+type Dictionary struct {
+	entries map[string]uint32
+	keys    []string
+	sorted  bool
+	lock    sync.RWMutex
+}
+
+// NewDictionary creates a new dictionary
+func NewDictionary() *Dictionary {
+	return &Dictionary{
+		entries: make(map[string]uint32),
+		keys:    make([]string, 0),
+		sorted:  true,
+	}
+}
+
+// LookupID returns the ID associated with the given symbol
+func (d *Dictionary) LookupID(symbol string) (uint32, bool) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
+	id, ok := d.entries[symbol]
+	return id, ok
+}
+
+// LookupSymbol returns the symbol associated with the given ID
+func (d *Dictionary) LookupSymbol(id uint32) (string, bool) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
+	if int(id) >= len(d.keys) {
+		return "", false
+	}
+	return d.keys[int(id)], true
+}
+
+// AddSymbol adds a new entry to this dictionary
+func (d *Dictionary) AddSymbol(key string) uint32 {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	id, ok := d.entries[key]
+	if !ok {
+		id = uint32(len(d.keys))
+		d.entries[key] = id
+		d.keys = append(d.keys, key)
+		d.sorted = false
+	}
+	return id
+}
+
+// Sort returns a new dictionary which is a sorted (and possibly renumbered) version of this one
+func (d *Dictionary) Sort() *Dictionary {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	result := &Dictionary{
+		entries: make(map[string]uint32),
+		keys:    make([]string, len(d.keys)),
+		sorted:  true,
+	}
+	copy(result.keys, d.keys)
+	sort.Strings(result.keys)
+	for idx, key := range result.keys {
+		result.entries[key] = uint32(idx)
+	}
+	return result
+}
+
+// Save writes this dictionary (must be sorted) to a file
+func (d *Dictionary) Save(filename string) error {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
+	if !d.sorted {
+		return errors.New("dictionary is not sorted")
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	bfile := bufio.NewWriter(file)
+	if _, err := bfile.Write(DictionaryV1Len); err != nil {
+		return err
+	}
+	if _, err := bfile.WriteString(DictionaryV1); err != nil {
+		return err
+	}
+	for _, key := range d.keys {
+		if err := binary.Write(bfile, binary.BigEndian, uint32(len(key))); err != nil {
+			return err
+		}
+		if _, err := bfile.WriteString(key); err != nil {
+			return err
+		}
+	}
+	if err := bfile.Flush(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDictionaryFromFile loads a dictionary object from a file
+func NewDictionaryFromFile(filename string) (*Dictionary, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	bfile := bufio.NewReader(file)
+	headerLen := make([]byte, 1)
+	bytesRead, err := bfile.Read(headerLen)
+	if err != nil {
+		return nil, err
+	}
+	if bytesRead != 1 {
+		return nil, errors.New("invalid dictionary header length")
+	}
+	version := make([]byte, headerLen[0])
+	bytesRead, err = bfile.Read(version)
+	if err != nil {
+		return nil, err
+	}
+	if bytesRead != len(version) {
+		return nil, errors.New("invalid dictionary header")
+	}
+	switch string(version) {
+	case DictionaryV1:
+		break
+	default:
+		return nil, errors.New("unknown dictionary version")
+	}
+
+	d := &Dictionary{
+		entries: make(map[string]uint32),
+		keys:    make([]string, 0),
+		sorted:  true,
+	}
+	lenBuf := make([]byte, 4)
+	for {
+		bytesRead, err = bfile.Read(lenBuf)
+		if err != nil && errors.Is(err, io.EOF) {
+			break
+		}
+		if bytesRead != 4 {
+			return nil, errors.New("corrupt dictionary")
+		}
+		bytesToRead := binary.BigEndian.Uint32(lenBuf)
+		keyBuf := make([]byte, bytesToRead)
+		bytesRead, err = bfile.Read(keyBuf)
+		if err != nil {
+			return nil, err
+		}
+		if bytesRead != len(keyBuf) {
+			return nil, errors.New("corrupt dictionary")
+		}
+		d.keys = append(d.keys, string(keyBuf))
+		d.entries[string(keyBuf)] = uint32(len(d.keys))
+	}
+	return d, nil
+}
